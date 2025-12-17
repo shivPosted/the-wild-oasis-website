40512b6ee5aa2df0851903906a1f18c17f7afa81
shivPosted <shivpratap621@gmail.com>
2025-12-18 01:09:43 +0530

    fetching data in server component

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,4 +1,16 @@
-function Page() {
-  return <div>Select your cabin</div>;
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <div>
+      Select your cabin
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Page;
